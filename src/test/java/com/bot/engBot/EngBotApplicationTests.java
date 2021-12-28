c1be0b7fea458b3e05f195e58c8e5318a8d29610
Mykhailo Afanasiev <afanasiev.m@ajax.systems>
2021-12-28 18:50:34 +0200

    build version

--- src/test/java/com/bot/engBot/EngBotApplicationTests.java
+++ src/test/java/com/bot/engBot/EngBotApplicationTests.java
@@ -1,13 +1,13 @@
-package com.bot.engBot;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EngBotApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.bot.engBot;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EngBotApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
