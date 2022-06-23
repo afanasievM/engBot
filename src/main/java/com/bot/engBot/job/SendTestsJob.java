bc8b2ec32755b2310cafca882df04c505bb57bca
Afanasiev Mykhailo <afanasiev.m@ajax.systems>
2022-06-23 15:06:57 +0300

    change time to test

--- src/main/java/com/bot/engBot/job/SendTestsJob.java
+++ src/main/java/com/bot/engBot/job/SendTestsJob.java
@@ -21,7 +21,7 @@ public class SendTestsJob {
         this.sendTestService = sendTestService;
     }
 
-    @Scheduled(cron = "0 0 8-21 * * *")
+    @Scheduled(cron = "0 0 9,12,15,18 ? * * *")
 //    @Scheduled(fixedDelay = 30000, initialDelay = 20000)
     public void chooseWords() {
         LocalDateTime start = LocalDateTime.now();
