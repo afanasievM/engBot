79bf7502793c6ce769a2b9b877a944929e64b6aa
afanasievM <g010203040>
2022-07-10 19:53:49 +0300

    Refactoring AddGroupCommand, AddGroupMemberCommand

--- src/main/java/com/bot/engBot/commands/AddGroupCommand.java
+++ src/main/java/com/bot/engBot/commands/AddGroupCommand.java
@@ -12,7 +12,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.util.Arrays;
 
-public class AddGroupCommand implements Command{
+public class AddGroupCommand implements Command {
     private final SendBotMessageService sendBotMessageService;
     private final GroupService groupService;
     final private Logger log = Logger.getLogger(AddGroupCommand.class);
@@ -28,33 +28,30 @@ public class AddGroupCommand implements Command{
     public void execute(Update update) {
         Long chatId = update.getMessage().getChatId();
         Long senderId = update.getMessage().getFrom().getId();
-        String cmd = update.getMessage().getText().replace("/add_group","");
-        String groupName = cmd.trim();
-        if (!groupName.equals("")){
-            groupService.findByGroupName(groupName).ifPresentOrElse(
-                    group -> {
-                        log.info("OLD Group");
-                        sendBotMessageService.sendMessage(chatId, "This group name exists.\nTry another name.");
-                    },
-                    () -> {
-                        log.info("NEW Group");
-                        sendBotMessageService.sendMessage(chatId, "You add new group:" + groupName
-                        + "\nYou can add members by command\n/add_group_member group name;@username\n" +
-                                "This user should use @vocabengbot.(/start)");
-                        Group group = new Group();
-                        group.setGroupName(groupName);
-                        group.setOwnerId(senderId);
-                        groupService.save(group);
-                        Group newGroup = groupService.findByGroupName(groupName).get();
-                        groupService.addGroupUser(newGroup.getId(),senderId);
-                    }
-            );
-        } else {
-
+        String groupName = update.getMessage().getText().replace("/add_group", "").trim();
+        if (groupName.isEmpty()) {
             sendBotMessageService.sendMessage(chatId, "Please use correct form: \n/add_group group name");
         }
-
-
-
+        groupService.findByGroupName(groupName).ifPresentOrElse(
+                group -> {
+                    log.info("OLD Group");
+                    sendBotMessageService.sendMessage(chatId, "This group name exists.\nTry another name.");
+                },
+                () -> {
+                    log.info("NEW Group");
+                    String message = String.format("You add new group: %s.\n" +
+                            "You can add members by command\n" +
+                            "/add_group_member group name;@username\n" +
+                            "This user should use @vocabengbot.(/start)", groupName);
+                    sendBotMessageService.sendMessage(chatId, message);
+                    Group group = new Group();
+                    group.setGroupName(groupName);
+                    group.setOwnerId(senderId);
+                    groupService.save(group);
+                    Group newGroup = groupService.findByGroupName(groupName).get();
+                    groupService.addGroupUser(newGroup.getId(), senderId);
+                }
+        );
     }
 }
+
--- src/main/java/com/bot/engBot/commands/AddGroupMemberCommand.java
+++ src/main/java/com/bot/engBot/commands/AddGroupMemberCommand.java
@@ -1,5 +1,6 @@
 package com.bot.engBot.commands;
 
+import com.bot.engBot.repository.entity.BotUser;
 import com.bot.engBot.repository.entity.Group;
 import com.bot.engBot.service.BotUserService;
 import com.bot.engBot.service.GroupService;
@@ -8,13 +9,19 @@ import org.apache.log4j.Logger;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import javax.print.DocFlavor;
 import java.util.Arrays;
+import java.util.Optional;
 
-public class AddGroupMemberCommand implements Command{
+public class AddGroupMemberCommand implements Command {
     private final SendBotMessageService sendBotMessageService;
     private final GroupService groupService;
     private final BotUserService botUserService;
     final private Logger log = Logger.getLogger(AddGroupMemberCommand.class);
+    private String groupName;
+    private String newMember;
+    private Long chatId;
+    private Long senderId;
 
 
     public AddGroupMemberCommand(SendBotMessageService sendBotMessageService, GroupService groupService, BotUserService botUserService) {
@@ -25,66 +32,86 @@ public class AddGroupMemberCommand implements Command{
 
     @Override
     public void execute(Update update) {
-        Long chatId = update.getMessage().getChatId();
-        Long senderId = update.getMessage().getFrom().getId();
-        String cmd = update.getMessage().getText().replace("/add_group_member","");
-        String groupName = null;
-        String newMember = null;
+        chatId = update.getMessage().getChatId();
+        senderId = update.getMessage().getFrom().getId();
+        parse(update.getMessage().getText());
+        if (groupName == null || newMember == null) {
+            sendBotMessageService.sendMessage(chatId, "Please use correct form: \n" +
+                    "/add_group_member group name;@username\n" +
+                    "This user should use @vocabengbot.(/start)");
+            return;
+        }
+        Group group = getGroup();
+        BotUser user = getUser();
+        if (user == null || !isValidGroup(group)) {
+            return;
+        }
+        addUserToGroup(group, user);
+    }
+
+    private void parse(String updateText) {
+        String cmd = updateText.replace("/add_group_member", "");
         try {
             String[] cmdStructure = cmd.split(";");
             log.info(Arrays.stream(cmdStructure).toArray().toString());
             groupName = cmdStructure[0].replace("@vocabengbot ", "").trim();
             newMember = cmdStructure[1].trim();
-            if (newMember.startsWith("@")) newMember = newMember.replaceFirst("@","");
-        } catch (Exception e){
+            if (newMember.startsWith("@")) newMember = newMember.replaceFirst("@", "");
+        } catch (Exception e) {
             log.info(e);
             sendBotMessageService.sendMessage(chatId, "Please use correct form: \n/add_group_member group name;@username" +
                     "\nThis user should use @vocabengbot.(/start)");
             return;
         }
-        if (!groupName.equals("") && !newMember.equals("")){
-            String finalGroupName = groupName;
-            String finalNewMember = newMember;
-            groupService.findByGroupName(groupName).ifPresentOrElse(
-                    group -> {
-                        log.info("OLD group Finded!");
-                        if (group.getOwnerId().equals(senderId)) {
-                            botUserService.findByUsername(finalNewMember).ifPresentOrElse(
-                                    user -> {
-                                        try {
-                                            groupService.addGroupUser(group.getId(),user.getId());
-                                            sendBotMessageService.sendMessage(chatId, "User <b>" + user.getUsername() + "</b> successfuly added to group <b>" +
-                                                    group.getGroupName() + "</b>.");
-                                            sendBotMessageService.sendMessage(user.getId(), "You have been added to group <b>"  + group.getGroupName() + "</b> by @"+
-                                                    botUserService.findByChatId(senderId).get().getUsername()  + ".");
-                                        } catch (DataIntegrityViolationException e){
-                                            sendBotMessageService.sendMessage(chatId, "User <b>" + user.getUsername() + "</b> is already exist in group <b>" +
-                                                    group.getGroupName() + "</b>.");
-                                            log.info(e);
-                                        }
+    }
 
-                                    },
-                                    () -> {
-                                        sendBotMessageService.sendMessage(chatId, "Can't find user in bot's database. " +
-                                                "\nThis user should use @vocabengbot.(/start)");
-                                    }
-                            );
-                        } else {
-                            sendBotMessageService.sendMessage(chatId, "Permissions denied. \nOnly group's admins or owner can add members to groups");
-                        }
-                    },
-                    () -> {
-                        log.info("Can't find group");
-                        sendBotMessageService.sendMessage(chatId, "Can't find group <b>" + finalGroupName + "</b>" +
-                                "\nTry tu use command /show_my_group or /show_my_own_groups to find anyone.");
-                    }
-            );
-        } else {
-            sendBotMessageService.sendMessage(chatId, "Please use correct form: \n/add_group_member group name;@username" +
-                    "\nThis user should use @vocabengbot.(/start)");
+    private Group getGroup() {
+        Optional<Group> optionalGroup = groupService.findByGroupName(groupName);
+        if (!optionalGroup.isPresent()) {
+            log.info("Can't find group");
+            String message = String.format("Can't find group <b>%s</b>\n" +
+                    "Try tu use command /show_my_group or /show_my_own_groups to find anyone.", groupName);
+            sendBotMessageService.sendMessage(chatId, message);
+            return null;
         }
+        return optionalGroup.get();
+    }
 
+    private boolean isValidGroup(Group group) {
+        if (group == null) {
+            return false;
+        }
+        log.info("OLD group Finded!");
+        if (!group.getOwnerId().equals(senderId)) {
+            sendBotMessageService.sendMessage(chatId, "Permissions denied. \n" +
+                    "Only group's admins or owner can add members to groups");
+            return false;
+        }
+        return true;
+    }
 
+    private BotUser getUser() {
+        Optional<BotUser> optionalUser = botUserService.findByUsername(newMember);
+        if (!optionalUser.isPresent()) {
+            sendBotMessageService.sendMessage(chatId,
+                    "Can't find user in bot's database.\n" +
+                            "This user should use @vocabengbot.(/start)");
+            return null;
+        }
+        return optionalUser.get();
+    }
 
+    private void addUserToGroup(Group group, BotUser user) {
+        try {
+            groupService.addGroupUser(group.getId(), user.getId());
+            sendBotMessageService.sendMessage(chatId, "User <b>" + user.getUsername() + "</b> successfuly added to group <b>" +
+                    group.getGroupName() + "</b>.");
+            sendBotMessageService.sendMessage(user.getId(), "You have been added to group <b>" + group.getGroupName() + "</b> by @" +
+                    botUserService.findByChatId(senderId).get().getUsername() + ".");
+        } catch (DataIntegrityViolationException e) {
+            sendBotMessageService.sendMessage(chatId, "User <b>" + user.getUsername() + "</b> is already exist in group <b>" +
+                    group.getGroupName() + "</b>.");
+            log.info(e);
+        }
     }
 }
