3761eb657251205777e4174a3877963966fd4f28
afanasievM <g010203040>
2022-07-10 19:12:20 +0300

    Refactoring CallBackServiceImpl

--- src/main/java/com/bot/engBot/service/CallBackServiceImpl.java
+++ src/main/java/com/bot/engBot/service/CallBackServiceImpl.java
@@ -5,11 +5,12 @@ import com.bot.engBot.repository.entity.Vocabulary;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.ParseMode;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import java.io.Serializable;
 
 @Service
 public class CallBackServiceImpl implements CallBackService {
@@ -29,61 +30,67 @@ public class CallBackServiceImpl implements CallBackService {
     public void callBackProcess(CallbackQuery callBack) {
         log.info(callBack.getMessage());
         Long chatId = callBack.getMessage().getChat().getId();
-        String wordFromMessage = callBack.getMessage().getText();
-        String wordFromMessageData = callBack.getData();
-        log.info(wordFromMessage);
+        removeButton(chatId, callBack);
+        String message = checkAnswer(chatId, callBack);
+        showAlertAnswer(message, callBack);
+    }
 
-        EditMessageReplyMarkup edit = new EditMessageReplyMarkup(chatId.toString(), callBack.getMessage().getMessageId(), callBack.getInlineMessageId(), null);
-
-        try {
-            engBot.execute(edit);
-        } catch (TelegramApiException e) {
-            log.info(e);
-        }
+    private void showAlertAnswer(String messageToSend, CallbackQuery callBack) {
         AnswerCallbackQuery answerCallBack = new AnswerCallbackQuery();
         answerCallBack.setCallbackQueryId(callBack.getId());
         answerCallBack.setShowAlert(true);
+        answerCallBack.setText(messageToSend);
+        execute(answerCallBack);
+    }
+
+    private String checkAnswer(Long chatId, CallbackQuery callBack) {
         String messageToSend;
+        String wordFromMessage = callBack.getMessage().getText();
+        String wordFromMessageData = callBack.getData();
+        log.info(wordFromMessage);
         Vocabulary testWord = vocabularyService.findByWordAndOwnerId(wordFromMessage, chatId).get();
         if (wordFromMessage.equals(wordFromMessageData)) {
-
-            messageToSend = testWord.getWord() + " -> " + testWord.getWordTranslation() + ".\nTRUE";
-            answerCallBack.setText(messageToSend);
-            try {
-                engBot.execute(answerCallBack);
-            } catch (Exception e) {
-                log.info(e);
-            }
-
+            messageToSend = String.format("%s -> %s.\nTRUE", testWord.getWord(), testWord.getWordTranslation());
             if (testWord.getRepeats() == 1) {
-                SendMessage sendMessage = new SendMessage();
-                sendMessage.enableMarkdown(true);
-                sendMessage.setChatId(testWord.getOwnerId().toString());
-                sendMessage.setParseMode(ParseMode.MARKDOWN);
-                sendMessage.setText("You learned *" + testWord.getWord() + "* -> *" + testWord.getWordTranslation() + "*!!!!");
-
-                try {
-                    engBot.execute(sendMessage);
-                } catch (TelegramApiException e) {
-                    //todo add logging to the project.
-                    log.error(e);
-                }
+                sendThatLearntWord(testWord);
             }
             vocabularyService.decreaseWordRepeats(testWord);
-
         } else {
-            messageToSend = "FALSE\n" + testWord.getWord() + " -> " + testWord.getWordTranslation() + "\n repeats reset to " + VocabularyServiceImpl.REPEATS;
-
-            answerCallBack.setText(messageToSend);
-
-            try {
-                engBot.execute(answerCallBack);
-            } catch (Exception e) {
-                log.info(e);
-            }
+            messageToSend = String.format("FALSE\n %s -> %s\n repeats reset to %d",
+                    testWord.getWord(),
+                    testWord.getWordTranslation(),
+                    VocabularyServiceImpl.REPEATS);
             vocabularyService.resetWord(testWord);
+        }
+        return messageToSend;
+    }
 
+    void sendThatLearntWord(Vocabulary testWord) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(testWord.getOwnerId().toString());
+        sendMessage.setParseMode(ParseMode.MARKDOWN);
+        String text = String.format("You learned *%s* -> *%s*!!!!",
+                testWord.getWord(),
+                testWord.getWordTranslation());
+        sendMessage.setText(text);
+        execute(sendMessage);
+    }
 
+    void removeButton(Long chatId, CallbackQuery callBack) {
+        EditMessageReplyMarkup edit = new EditMessageReplyMarkup(
+                chatId.toString(),
+                callBack.getMessage().getMessageId(),
+                callBack.getInlineMessageId(),
+                null);
+        execute(edit);
+    }
+
+    private <T extends Serializable, Method extends BotApiMethod<T>> void execute(Method method) {
+        try {
+            engBot.execute(method);
+        } catch (Exception e) {
+            log.info(e);
         }
     }
 }
