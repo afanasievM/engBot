af2256b54eb7caf79208e5a67e2099d8a60951b9
Afanasiev Mykhailo <afanasiev.m@ajax.systems>
2022-07-11 13:03:37 +0300

    Refactoring ShowMyUnstudiedWordsCommand

--- src/main/java/com/bot/engBot/commands/ShowMyUnstudiedWordsCommand.java
+++ src/main/java/com/bot/engBot/commands/ShowMyUnstudiedWordsCommand.java
@@ -4,14 +4,12 @@ import com.bot.engBot.repository.entity.Vocabulary;
 import com.bot.engBot.service.SendBotMessageService;
 import com.bot.engBot.service.VocabularyService;
 import org.apache.log4j.Logger;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
-public class ShowMyUnstudiedWordsCommand implements Command{
+public class ShowMyUnstudiedWordsCommand implements Command {
     private final SendBotMessageService sendBotMessageService;
     private final VocabularyService vocabularyService;
     final private Logger log = Logger.getLogger(ShowMyUnstudiedWordsCommand.class);
@@ -25,19 +23,19 @@ public class ShowMyUnstudiedWordsCommand implements Command{
     @Override
     public void execute(Update update) {
         Long chatId = update.getMessage().getChatId();
-        List<Vocabulary> vocabularyList = new ArrayList<>();
-        vocabularyList.addAll(vocabularyService.findAllByOwnerIdAndActiveFalse(update.getMessage().getFrom().getId()));
+        List<Vocabulary> vocabularyList = vocabularyService.findAllByOwnerIdAndActiveFalse(update.getMessage().
+                getFrom().getId());
         String wordsList = "";
-        for (Vocabulary word:vocabularyList) {
-            String currentLine = word.getWord() + ": " + word.getRepeats() +"\n";
-            if (wordsList.toCharArray().length + currentLine.toCharArray().length > messageSize){
-                sendBotMessageService.sendMessage(chatId,wordsList);
+        for (Vocabulary word : vocabularyList) {
+            String currentLine = String.format("%s: %d\n", word.getWord(), word.getRepeats());
+            if (wordsList.toCharArray().length + currentLine.toCharArray().length > messageSize) {
+                sendBotMessageService.sendMessage(chatId, wordsList);
                 wordsList = "";
             }
             wordsList += currentLine;
         }
-        sendBotMessageService.sendMessage(chatId,wordsList);
+        sendBotMessageService.sendMessage(chatId, wordsList);
         String totalMessage = "Total: " + vocabularyList.size();
-        sendBotMessageService.sendMessage(chatId,totalMessage);
+        sendBotMessageService.sendMessage(chatId, totalMessage);
     }
 }
