a65e3a17af5a8cf3134181df18c2369e761a015c
Afanasiev Mykhailo <afanasiev.m@ajax.systems>
2022-07-11 12:29:51 +0300

    Refactoring ShowGroupMembers

--- src/main/java/com/bot/engBot/commands/ShowGroupMemberCommand.java
+++ src/main/java/com/bot/engBot/commands/ShowGroupMemberCommand.java
@@ -1,59 +1,57 @@
 package com.bot.engBot.commands;
 
+import com.bot.engBot.repository.entity.Group;
 import com.bot.engBot.service.*;
 import org.apache.log4j.Logger;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-import java.util.ArrayList;
+import java.util.List;
 
-public class ShowGroupMemberCommand implements Command{
-    private final SendBotMessageService sendBotMessageService;
-    private final GroupService groupService;
+public class ShowGroupMemberCommand extends GroupCommand implements Command {
     private final BotUserService botUserService;
     final private Logger log = Logger.getLogger(ShowGroupMemberCommand.class);
 
 
     public ShowGroupMemberCommand(SendBotMessageService sendBotMessageService, BotUserService botUserService, GroupService groupService) {
-        this.sendBotMessageService = sendBotMessageService;
-        this.groupService = groupService;
+        super(sendBotMessageService, groupService);
         this.botUserService = botUserService;
     }
 
     @Override
     public void execute(Update update) {
-        Long chatId = update.getMessage().getChatId();
-        Long senderId = update.getMessage().getFrom().getId();
-        String groupName = update.getMessage().getText().replace("/show_group_users","").trim();
-        if (groupName.equals("")) return;
+        chatId = update.getMessage().getChatId();
+        senderId = update.getMessage().getFrom().getId();
+        groupName = update.getMessage().getText().replace("/show_group_users", "").trim();
+        if (groupName.isEmpty()) {
+            return;
+        }
+        Group group = getGroup();
+        if (group == null) {
+            return;
+        }
+        List<Long> groupUsers = groupService.getGroupUsers(group.getId());
+        List<Long> groupTeachers = groupService.getGroupTeachers(group.getId());
+        if (!groupUsers.contains(senderId) || !groupTeachers.contains(senderId)) {
+            sendBotMessageService.sendMessage(chatId, "Permissions denied. \n" +
+                    "Only group's members or teachers can show info about group.");
+        }
+        sendGroupMembers(groupUsers);
+    }
 
-        groupService.findByGroupName(groupName).ifPresentOrElse(
-                group -> {
-                    ArrayList<Long> groupUsers = (ArrayList<Long>) groupService.getGroupUsers(group.getId());
-                    ArrayList<Long> groupTeachers = (ArrayList<Long>) groupService.getGroupTeachers(group.getId());
-                    StringBuilder usersList = new StringBuilder();
-                    if (groupUsers.contains(senderId)||groupTeachers.contains(senderId)) {
-                        for (Long userId:groupUsers) {
-                            botUserService.findByChatId(userId).ifPresentOrElse(
-                                    user ->{
-                                        usersList.append("@" + user.getUsername() + "\n");
-                                    },
-                                    ()->{
-                                        log.info("can't find user");
-                                    }
-                            );
-                        }
-                        sendBotMessageService.sendMessage(chatId, usersList.toString());
-                        sendBotMessageService.sendMessage(chatId, "Total:" + groupUsers.size());
-                    } else {
-                        sendBotMessageService.sendMessage(chatId, "Permissions denied. \nOnly group's members or teachers can show info about group.");
+    private void sendGroupMembers(List<Long> groupUsers) {
+        StringBuilder usersList = new StringBuilder();
+        for (Long userId : groupUsers) {
+            botUserService.findByChatId(userId).ifPresentOrElse(
+                    user -> {
+                        usersList.append("@" + user.getUsername() + "\n");
+                    },
+                    () -> {
+                        log.info("can't find user");
                     }
-                },
-                () -> {
-                    log.info("Can't find group");
-                    sendBotMessageService.sendMessage(chatId, "Can't find group <b>" + groupName + "</b>" +
-                            "\nTry tu use command /show_my_group or /show_my_own_groups to find anyone.");
-                }
-        );
-
+            );
+        }
+        sendBotMessageService.sendMessage(chatId, usersList.toString());
+        sendBotMessageService.sendMessage(chatId, "Total:" + groupUsers.size());
     }
 }
+
